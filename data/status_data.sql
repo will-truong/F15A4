b3e2feb5aedcc2c2e06073f06ca95168dab14301
Cedric Charly <cedric.charly@gmail.com>
2015-11-20 00:02:02 -0600

    Added insert data for F15A4_status table

---
+++ data/status_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'Entered', 'The RFE has been created but has not yet been submitted for approval.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'Submitted', 'The RFE has been submitted to the Lab System Administrator for approval.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'Returned', 'The RFE has been returned for further information or clarification. Once submitted again, it will follow the same routing as an Entered RFE.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'Recalled', 'The requestor has recalled an RFE that has not yet reached final approval. Once submitted again, it will follow the same routing as an Entered RFE.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'Rejected', 'The RFE has been rejected and cannot be implemented.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'SA Approved', 'The Lab System Administrator has approved the RFE; it has been submitted for Lab Director approval.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'LD Approved', 'The Lab Director has approved the RFE; it has been submitted for Network Security Panel approval.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'CH Approved', 'The Lab Director has approved the RFE; it has been submitted to the Chairperson of Security Panel approval.');
+INSERT INTO F15A4_status(status_id, status_type, description) VALUES(F15A4_status_seq.nextval, 'Final Approved', 'The Executive Directors Office has given final approval for the RFE and it may be implemented.');
