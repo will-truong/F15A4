179ae81c140149ccba40549a3a3a7229bd733d0e
Cedric Charly <cedric.charly@gmail.com>
2015-11-20 00:33:59 -0600

    Added lab inserts for data

---
+++ data/lab_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Encryption', 'ENCR');
+INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Propulsion', 'PROP');
+INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Electricity', 'ELEC');
+INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Gravity', 'GRAV');
+INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Magnetism', 'MAG');
