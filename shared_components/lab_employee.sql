c6f95782fe10e7e54c0433608fa529fb08b5dddc
Cedric Charly <cedric.charly@gmail.com>
2015-11-21 15:28:36 -0600

    Saved queries to build shared_components

---
+++ shared_components/lab_employee.sql
@@ -0,0 +1,3 @@
+select employee_name as d, employee_id as r 
+from F15A4_emp 
+where F15A4_LAB_LAB_ID = :P2_LAB order by 1;
---
+++ shared_components/labs.sql
@@ -0,0 +1,3 @@
+select lab_name as l, lab_id as r 
+from F15A4_lab 
+order by 1;
