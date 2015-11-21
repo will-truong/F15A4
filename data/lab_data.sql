626d40da30f4b1b7c68d0f25eac5aab98d388ccf
Cedric Charly <cedric.charly@gmail.com>
2015-11-21 01:18:13 -0600

    Created Admin lab for login

--- data/lab_data.sql
+++ data/lab_data.sql
@@ -3,3 +3,4 @@ INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Propulsion', 'PROP');
 INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Electricity', 'ELEC');
 INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Gravity', 'GRAV');
 INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Magnetism', 'MAG');
+INSERT INTO F15A4_lab(lab_name, lab_code) VALUES('Administration', 'ADM');
